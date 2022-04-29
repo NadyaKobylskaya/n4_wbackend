19b3763d432f65276059e2d7637cafc330328008
NadyaKobylskaya <90647507+NadyaKobylskaya@users.noreply.github.com>
2022-04-29 17:42:32 +0300

    Update index.php

--- backend_4/index.php
+++ backend_4/index.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 	if (!empty($_COOKIE['save'])) {
 		setcookie("save", '', time() - 60 * 60 * 24);
